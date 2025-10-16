54599a61762d0c2d03bcfe397f4fd26198e97644
Ștefăniță Mitran <stefanita.mitran@hotmail.com>
2025-10-16 18:21:25 +0000

    added roater and shadcn component library

--- src/ui/Login.tsx
+++ src/ui/Login.tsx
@@ -1,12 +1,12 @@
 import { Button } from "@/components/ui/button";
 
-function App() {
+function Login() {
 
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>hai dă click, te rog!</Button>
+      <Button>Cică e login!</Button>
     </div>
   );
 }
 
-export default App;
+export default Login;
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tailwindcss()],
   server: {
     // bind to all addresses so the server is reachable from the host when running inside a devcontainer
     host: true, // same as '0.0.0.0'
     port: 5173,
   },
+  resolve: {
+  alias: {
+    "@": path.resolve(__dirname, "./src"),
+  },
+},
 });
