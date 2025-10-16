2ec19193b27bad8773a0265767d78d17ae0bf50b
Ștefăniță-Alexandru Mitran <stefanita.mitran@hotmail.com>
2025-10-16 17:04:55 +0000

    added devcontainers configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    // bind to all addresses so the server is reachable from the host when running inside a devcontainer
+    host: true, // same as '0.0.0.0'
+    port: 5173,
+  },
 });
